2b608b104bc31ed6252d75296ba93154af90db0a
yonatan-h <yon019hl@gmail.com>
2023-02-18 22:38:17 +0300

    -

--- run-mirkusve.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home/yonatan/Desktop/javoby/jsfo8/mirkusve
-npm start
-
-read -p "runninnn"
--- upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home/yonatan/Desktop/javoby/leetcode/competitive-programming
-git add .
-git commit -m "answer"
-git push
