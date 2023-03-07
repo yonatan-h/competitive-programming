129a0e085b96e1e7f9435717f803d29395cd73ce
yonatan-h <99685585+yonatan-h@users.noreply.github.com>
2023-03-07 22:59:38 +0300

    Create Sum of Subarray Minimums.py

---
+++ week12-after-admission/Sum of Subarray Minimums.py
@@ -0,0 +1,28 @@
+class Solution:
+    def sumSubarrayMins(self, nums: List[int]) -> int:
+        nums = [-2] + nums + [-1]
+        stack = []
+        min_sums = 0
+
+        for i in range(len(nums)):
+            while stack and  nums[i] < nums[stack[-1]]:
+                popped_index = stack.pop()
+
+                with_right = i - popped_index
+                with_left = popped_index - stack[-1]
+                
+                num_subarrays = with_right * with_left
+                min_sums += nums[popped_index] * num_subarrays
+
+            stack.append(i)
+
+        return (min_sums) % ((10 **9) + 7)
+
+                    
+
+
+
+
+
+ #40min
+#5sub
