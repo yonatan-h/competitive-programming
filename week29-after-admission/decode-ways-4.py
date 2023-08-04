eb4a5eb1b0d483b633ad28e0684c8bdf8a427bb3
yonatan-h <99685585+yonatan-h@users.noreply.github.com>
2023-08-04 18:40:49 +0300

    Uploaded By Mirkusve

---
+++ week29-after-admission/decode-ways-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        @cache
+        def pick(index):
+            if index >= len(s):
+                return 1
+            count = 0
+            #0x
+            if s[index]=="0":
+                return 0
+                
+            #a
+            count += pick(index+1)
+            #ab
+            if index+1 < len(s):
+                two_digits = int(s[index]+s[index+1])
+                
+                if two_digits <= 26:
+                    count += pick(index+2)
+            
+            return count
+        
+        return pick(0)
